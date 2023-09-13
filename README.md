a8d2eca4df80d6fdeae2464fd8573f449c6ecc41
Marc Klemp <110910273+marcklemp@users.noreply.github.com>
2023-09-13 23:10:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# vips.el
+
+`vips.el` is a powerful Emacs interface for OpenAI's GPT API and DeepL's translation API. This tool allows you to leverage the capabilities of these APIs directly within your Emacs environment.
+
+## Key Features
+
+- Work directly with OpenAI's GPT-4 and GPT-3.5-turbo models in Emacs.
+- Customize parameters for OpenAI's API, including max tokens, temperature, top-p, frequency penalty, and presence penalty to fine-tune the output.
+- Translate text seamlessly using the DeepL API.
+- Choose your target language for translation.
+
+## Installation Steps
+
+1. Download `vips.el` to your local system.
+2. Add the directory containing `vips.el` to your `load-path` using `(add-to-list 'load-path "/path/to/your/directory")`.
+3. Load the package with `(require 'vips)`.
+
+## How to Use
+
+Activate `vips-mode` in your current buffer by executing `M-x vips-mode`.
+
+Once `vips-mode` is enabled, you can use the following shortcuts:
+
+- `C-c <left>`: Send selected region or paragraph to OpenAI's GPT-4 model and append the result at the end of the region.
+- `C-c <right>`: Send selected region or paragraph to OpenAI's GPT-3.5-turbo model and append the result at the end of the region.
+- `C-c <down>`: Send selected region or paragraph to a user-selected OpenAI model and append the result at the end of the region.
+- `C-c C-a C-v`: Send entire buffer to OpenAI's GPT-4 model and append the result at the end of the buffer.
+- `C-c C-a C-c`: Send region defined from the top of the buffer to the current point to OpenAI's GPT-4 model and append the result at the end of the region.
+- `C-c SPC`: Select output language and translate selected region or paragraph using DeepL API and append the result at the end of the region.
+
+## Important Notes
+
+To use this tool, you must have valid API keys for GPT and/or DeepL.
+
+Add the following code to your config.el file:
+
+```emacs-lisp
+(setq openai-api-key "<your-openai-key-here>")
+(setq deepl-api-key "<your-deepl-key-here>")
+```
+
+Replace <your-openai-key-here> and <your-deepl-key-here> with your actual API keys.
+
+The `vips-languages` variable contains the list of language codes available for the translation function, initially set to include Danish ("DA") and English ("EN"). To modify this list, you can add a `setq` form to your Emacs configuration file (.emacs or init.el), like so:
+
+```emacs-lisp
+(setq vips-languages '("DA" "EN" "DE" "FR"))
+```
+
+This will add German ("DE") and French ("FR") to the list of available languages.
+
+## License
+
+This program is distributed as free software. You can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. For more details, please refer to the [LICENSE](LICENSE) file.
+
+## Acknowledgements
+
+This project was inspired by `aide.el` by Junji Zhi. The POST-related code was adapted and extended from the original code in `aide.el`. We thank Junji Zhi for their contribution to the open source community. `aide.el` is licensed under the GPL license.
---
+++ vips.el
@@ -0,0 +1,263 @@
+;;; vips.el --- An Emacs front end for OpenAI's GPT API and DeepL's translation API  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Marc Klemp
+;; Keywords: GPT, DeepL
+
+;; POST-related code adapted and extended from `aide.el` by Junji Zhi.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see https://www.gnu.org/licenses/.
+
+(require 'request)
+
+;; General settings
+
+(defgroup vips nil
+  "vips.el custom settings"
+  :group 'external
+  :prefix "vips-")
+
+(defconst vips-openai-api-url "https://api.openai.com/v1/chat/completions")
+(defconst vips-deepl-api-url "https://api.deepl.com/v2/translate")
+(defconst vips-recenter-position -2
+  "Position to recenter the view after inserting the result.")
+
+;; OpenAI model settings
+
+(defcustom vips-max-tokens 4000
+  "The max-tokens paramater that vips.el sends to OpenAI API."
+  :type 'integer
+  :group 'vips)
+
+(defcustom vips-temperature 0.7
+  "The temperature paramater that vips.el sends to OpenAI API."
+  :type 'float
+  :group 'vips)
+
+(defcustom vips-top-p 0.1
+  "The top-p paramater that vips.el sends to OpenAI API."
+  :type 'float
+  :group 'vips)
+
+(defcustom vips-frequency-penalty 0.1
+  "The frequency_penalty paramater that vips.el sends to OpenAI API."
+  :type 'float
+  :group 'vips)
+
+(defcustom vips-presence-penalty 0.1
+  "The presence_penalty paramater that vips.el sends to OpenAI API."
+  :type 'float
+  :group 'vips)
+
+;; DeepL-related settings
+(defcustom vips-languages '("DA" "EN")
+  "List of languages available for selection."
+  :type '(repeat string)
+  :group 'vips)
+
+(defvar-local vips-selected-language "DA"
+  "Selected target language for translation.")
+
+(defun vips-deepl-translate (api-key prompt)
+  "Return translated text from Deepl API. \n\n API-KEY is Deepl API key. \n\n PROMPT is text string sent to the API."
+  (vips-select-language)
+  (let ((result nil)
+        (auth-value (format "DeepL-Auth-Key %s" api-key)))
+    (request
+      vips-deepl-api-url
+      :type "POST"
+      :data `(("text" . ,prompt) ("target_lang" . ,vips-selected-language))
+      :parser 'json-read
+      :headers `(("Authorization" . ,auth-value))
+      :sync t
+      :success (cl-function
+                (lambda (&key data &allow-other-keys)
+                  (setq result (concat "\n" (alist-get 'text (elt (alist-get 'translations data) 0)))))))
+   result))
+
+(defun vips-select-language ()
+  "Prompt user to select a language and set the global variable 'vips-selected-language' to the selected language code."
+  (interactive)
+  (setq vips-selected-language (completing-read "Select target language: " vips-languages)))
+
+(defun vips--deepl-translate-string (string)
+  (vips-deepl-translate deepl-api-key string))
+
+(defun vips-translate (start end)
+  "Send region or paragraph to Deepl and insert result to end of region. \n\n START and END are selected region boundaries."
+  (interactive "r")
+  (vips-process-region start end 'vips--deepl-translate-string))
+
+(defun vips--openai-api-request (api-key model prompt)
+  "Return prompt answer from OpenAI API. \n\n API-KEY is OpenAI API key. \n\n MODEL is the model string, e.g., \"gpt-4\". \n\n PROMPT is prompt string sent to the API."
+  (let* ((auth-value (format "Bearer %s" api-key))
+         (insertion-marker (make-marker)) ;; create a new marker
+         (prompt-end (+ (region-beginning) (length prompt))) ;; calculate the end of the prompt
+         (current-buffer (current-buffer))) ;; capture the current buffer
+    (set-marker insertion-marker prompt-end current-buffer) ;; set the marker at the end of the prompt in the current buffer
+    (message "Request sent, waiting for response.") ;; display message after request is sent
+    (request
+      vips-openai-api-url
+      :type "POST"
+      :data (json-encode `(("model" . ,model)
+                           ("messages" . ((("role" . "system") ("content" . "Answer or complete if input is a question or instruction; otherwise, complete the input."))
+                                         (("role" . "user") ("content" . ,prompt))))
+                           ("max_tokens" . ,vips-max-tokens)
+                           ("temperature" . ,vips-temperature)
+                           ("frequency_penalty" . ,vips-frequency-penalty)
+                           ("presence_penalty" . ,vips-presence-penalty)
+                           ("top_p" . ,vips-top-p)))
+      :headers `(("Authorization" . ,auth-value) ("Content-Type" . "application/json"))
+      :sync nil ;; make the request asynchronous
+      :parser 'json-read
+      :success (cl-function
+                (lambda (&key data &allow-other-keys)
+                  (with-current-buffer current-buffer ;; switch to the captured buffer
+                    (goto-char insertion-marker) ;; go to the position of the marker
+                    (insert (concat "\n" (alist-get 'content (alist-get 'message (elt (alist-get 'choices data) 0))))))
+                  (message "Response inserted."))) ;; display message after response is inserted
+      :error (cl-function
+              (lambda (&key error-thrown &allow-other-keys)
+                (message "Error: %S" error-thrown))))
+  nil)) ;; return nil explicitly
+
+(defun vips--openai-chat (model prompt)
+  (vips--openai-api-request openai-api-key model prompt))
+
+(defun vips-chat-region (start end model)
+  "Send region or paragraph to OpenAI and insert result to end of region. \n\n START and END are selected region boundaries."
+  (interactive "r")
+  (vips-process-region start end (lambda (region) (vips--openai-chat model region))))
+
+(defun vips-chat-region-gpt-4 (start end)
+  "Call `vips-chat-region' with the gpt-4 model argument passed in."
+  (interactive "r")
+  (vips-chat-region start end "gpt-4"))
+
+(defun vips-chat-region-gpt-3.5-turbo (start end)
+  "Call `vips-chat-region' with the gpt-3.5-turbo model argument passed in."
+  (interactive "r")
+  (vips-chat-region start end "gpt-3.5-turbo"))
+
+(defun vips-chat-region-select-model (start end)
+  "Call `vips-chat-region' with a user-selected model."
+  (interactive "r")
+  (let* ((models '("gpt-4" "gpt-3.5-turbo" "gpt-3.5-turbo-16k"))
+         (selected-model (completing-read "Select a model: " models nil t)))
+    (vips-chat-region start end selected-model)))
+
+(defun vips-process-region (start end process-fn)
+  "Process the region or paragraph using PROCESS-FN."
+  (if (use-region-p)
+      (let* ((region (buffer-substring-no-properties start end))
+             (result (funcall process-fn region)))
+        (if result
+            (progn
+              (goto-char end)
+              (insert "" result "\n")
+              (push-mark start 'no-message)
+              (setq deactivate-mark nil)
+              (unless (pos-visible-in-window-p (point))
+                (recenter vips-recenter-position)))
+          (message "Empty result or not yet received")))
+    (mark-paragraph)
+    (let* ((region (buffer-substring-no-properties (region-beginning) (region-end)))
+           (result (funcall process-fn region)))
+      (if result
+          (progn
+            (goto-char (region-end))
+            (insert result "\n")
+            (push-mark (region-beginning) 'no-message)
+            (setq deactivate-mark nil)
+            (unless (pos-visible-in-window-p (point))
+              (recenter vips-recenter-position)))
+        (message "Empty result or not yet received")))))
+
+(defun vips-process-region (start end process-fn)
+  "Process the region or paragraph using PROCESS-FN."
+  (if (use-region-p)
+      (let* ((region (buffer-substring-no-properties start end))
+             (result (funcall process-fn region)))
+        (if result
+            (progn
+              (goto-char end)
+              (insert "" result "\n")
+              (push-mark start 'no-message)
+              (setq deactivate-mark nil)
+              (unless (pos-visible-in-window-p (point))
+                (recenter vips-recenter-position))
+              (message "Response inserted.")) ;; Display message after response is inserted
+          (message "Empty result or not yet received")))
+    (mark-paragraph)
+    (let* ((region (buffer-substring-no-properties (region-beginning) (region-end)))
+           (result (funcall process-fn region)))
+      (if result
+          (progn
+            (goto-char (region-end))
+            (insert result "\n")
+            (push-mark (region-beginning) 'no-message)
+            (setq deactivate-mark nil)
+            (unless (pos-visible-in-window-p (point))
+              (recenter vips-recenter-position))
+            (message "Response inserted.")) ;; Display message after response is inserted
+        (message "Empty result or not yet received")))))
+
+(defun mark-and-run-vips-chat-region-gpt-4 ()
+  "Mark the entire buffer and run 'vips-chat-region-gpt-4'."
+  (interactive)
+  (let* ((start (point-min))
+         (end (point-max)))
+    (if (not (region-active-p)) ; check if a region is active
+        (push-mark start 'no-message)) ; if not, set mark at start position
+    (let* ((region (buffer-substring-no-properties start end))
+           (result (vips--openai-chat "gpt-4" region)))
+      (if result
+          (progn
+            (goto-char end)
+            (insert "" result "\n")
+            (setq deactivate-mark nil) ; keep selection active
+            (unless (pos-visible-in-window-p (point)) ; check if the end of the selection is visible
+              (recenter vips-recenter-position))) ; move view so that the end of the region is towards the bottom
+        (message "Empty result or not yet received")))))
+
+(defun mark-and-run-vips-chat-region-gpt-4-to-current ()
+  "Mark the text from the start of the buffer to the the current position and run 'vips-chat-region-gpt-4'."
+  (interactive)
+  (let* ((start (point-min))
+         (end (point)))
+    (if (not (region-active-p)) ; check if a region is active
+        (push-mark start 'no-message)) ; if not, set mark at start position
+    (let* ((region (buffer-substring-no-properties start end))
+           (result (vips--openai-chat "gpt-4" region)))
+      (if result
+          (progn
+            (goto-char end)
+            (insert "" result "\n")
+            (setq deactivate-mark nil) ; keep selection active
+            (unless (pos-visible-in-window-p (point)) ; check if the end of the selection is visible
+              (recenter vips-recenter-position))) ; move view so that the end of the region is towards the bottom
+        (message "Empty result or not yet received")))))
+
+(define-minor-mode vips-mode
+  "Minor mode to use vips functions."
+  :lighter " Vips"
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "C-c <left>") 'vips-chat-region-gpt-4)
+            (define-key map (kbd "C-c <right>") 'vips-chat-region-gpt-3.5-turbo)
+            (define-key map (kbd "C-c <down>") 'vips-chat-region-select-model)
+            (define-key map (kbd "C-c C-a C-c") 'mark-and-run-vips-chat-region-gpt-4-to-current)
+            (define-key map (kbd "C-c C-a C-v") 'mark-and-run-vips-chat-region-gpt-4)
+            (define-key map (kbd "C-c SPC") 'vips-translate)
+            map))
+
+(provide 'vips)
